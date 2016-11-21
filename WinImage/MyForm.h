555294e0a57b4c5be8d00b1a93d69ef723a1b703
hkitty <hkitty@HKITTYPC>
2016-11-21 17:51:19 +0200

    Init

--- WinImage/MyForm.h
+++ WinImage/MyForm.h
@@ -8,6 +8,8 @@ namespace WinImage {
 	using namespace System::Windows::Forms;
 	using namespace System::Data;
 	using namespace System::Drawing;
+	using namespace System::Threading;
+	using namespace System::Globalization;
 
 	/// <summary>
 	/// Summary for MyForm
@@ -34,6 +36,18 @@ namespace WinImage {
 				delete components;
 			}
 		}
+	private: System::Windows::Forms::MenuStrip^  menuStrip1;
+	protected:
+	private: System::Windows::Forms::ToolStripMenuItem^  fileToolStripMenuItem;
+	private: System::Windows::Forms::Button^  button1;
+	private: System::Windows::Forms::ToolStripMenuItem^  exitToolStripMenuItem;
+	private: System::Windows::Forms::ToolStripMenuItem^  editToolStripMenuItem;
+
+
+
+	protected:
+
+
 
 	private:
 		/// <summary>
@@ -48,12 +62,67 @@ namespace WinImage {
 		/// </summary>
 		void InitializeComponent(void)
 		{
-			this->components = gcnew System::ComponentModel::Container();
-			this->Size = System::Drawing::Size(300,300);
-			this->Text = L"MyForm";
-			this->Padding = System::Windows::Forms::Padding(0);
+			System::ComponentModel::ComponentResourceManager^  resources = (gcnew System::ComponentModel::ComponentResourceManager(MyForm::typeid));
+			this->menuStrip1 = (gcnew System::Windows::Forms::MenuStrip());
+			this->fileToolStripMenuItem = (gcnew System::Windows::Forms::ToolStripMenuItem());
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->exitToolStripMenuItem = (gcnew System::Windows::Forms::ToolStripMenuItem());
+			this->editToolStripMenuItem = (gcnew System::Windows::Forms::ToolStripMenuItem());
+			this->menuStrip1->SuspendLayout();
+			this->SuspendLayout();
+			// 
+			// menuStrip1
+			// 
+			resources->ApplyResources(this->menuStrip1, L"menuStrip1");
+			this->menuStrip1->Items->AddRange(gcnew cli::array< System::Windows::Forms::ToolStripItem^  >(2) {
+				this->fileToolStripMenuItem,
+					this->editToolStripMenuItem
+			});
+			this->menuStrip1->Name = L"menuStrip1";
+			// 
+			// fileToolStripMenuItem
+			// 
+			resources->ApplyResources(this->fileToolStripMenuItem, L"fileToolStripMenuItem");
+			this->fileToolStripMenuItem->DropDownItems->AddRange(gcnew cli::array< System::Windows::Forms::ToolStripItem^  >(1) { this->exitToolStripMenuItem });
+			this->fileToolStripMenuItem->Name = L"fileToolStripMenuItem";
+			// 
+			// button1
+			// 
+			resources->ApplyResources(this->button1, L"button1");
+			this->button1->Name = L"button1";
+			this->button1->UseVisualStyleBackColor = true;
+			this->button1->Click += gcnew System::EventHandler(this, &MyForm::button1_Click_1);
+			// 
+			// exitToolStripMenuItem
+			// 
+			resources->ApplyResources(this->exitToolStripMenuItem, L"exitToolStripMenuItem");
+			this->exitToolStripMenuItem->Name = L"exitToolStripMenuItem";
+			// 
+			// editToolStripMenuItem
+			// 
+			resources->ApplyResources(this->editToolStripMenuItem, L"editToolStripMenuItem");
+			this->editToolStripMenuItem->Name = L"editToolStripMenuItem";
+			// 
+			// MyForm
+			// 
+			resources->ApplyResources(this, L"$this");
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->Controls->Add(this->button1);
+			this->Controls->Add(this->menuStrip1);
+			this->Name = L"MyForm";
+			this->menuStrip1->ResumeLayout(false);
+			this->menuStrip1->PerformLayout();
+			this->ResumeLayout(false);
+			this->PerformLayout();
+
 		}
 #pragma endregion
+	private: System::Void button1_Click_1(System::Object^  sender, System::EventArgs^  e) {
+		Thread::CurrentThread->CurrentCulture = gcnew CultureInfo("en-US");
+		Thread::CurrentThread->CurrentUICulture = gcnew CultureInfo("en-US");
+		
+		this->Controls->Clear();
+		this->InitializeComponent();
+	}
 	};
 }
